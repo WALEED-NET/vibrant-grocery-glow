93ddbbe442148cadee99a815bc395f6c7361435b
Waleed Bensumaidea <waleed.com145@gmail.com>
2025-06-29 13:37:35 +0300

    add blazor

---
+++ Blazor/SmartShop_Solution/SmartShop/SmartShop.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using SmartShop.Shared.Services;
+
+namespace SmartShop.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
